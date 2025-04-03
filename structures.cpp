f926556ed264b183edc05adc02522158e2d3f0f2
manojhunasimarad <89728596+manojhunasimarad@users.noreply.github.com>
2025-04-03 15:37:34 +0530

    Update structures.cpp
    added arrays of structure to illustrate the deck of cards

--- structures.cpp
+++ structures.cpp
@@ -11,6 +11,12 @@ struct emp
     int id;
     char name[50];
 };
+struct cards
+{
+    int facevalue;
+std::string shape;
+std::string color;
+};
 int main() {
    struct rectangle r = {10,20};
     // r.length=10;
@@ -22,5 +28,13 @@ int main() {
     strcpy(e.name, "manoj"); 
     std::cout<<e.id<<std::endl;
     std::cout<<e.name<<std::endl;
+    struct cards deck[52];
+    deck[0].facevalue=1;//for ace
+    deck[0].shape="heart";
+    deck[0].color="red";
+    std::cout << "Card 0 details:" << std::endl;
+    std::cout << "Face Value: " << deck[0].facevalue << std::endl;
+    std::cout << "Shape: " << deck[0].shape << std::endl;
+    std::cout << "Color: " << deck[0].color << std::endl;
     return 0;
 }
