82f363c346671197ca6b33d602e118db30e1dbfd
manojhunasimarad <89728596+manojhunasimarad@users.noreply.github.com>
2025-04-03 15:25:20 +0530

    Update structures.cpp
    creating the struct where multiple data types can be stored

--- structures.cpp
+++ structures.cpp
@@ -1,14 +1,26 @@
+// Online C++ compiler to run C++ program online
 #include <iostream>
+#include <cstring>
 struct rectangle
 {
     int length;
     int breadth;
 };
+struct emp
+{
+    int id;
+    char name[50];
+};
 int main() {
-    struct rectangle r;// struct rectangle r = {10,20};
-    r.length=10;
-    r.breadth=20;
+   struct rectangle r = {10,20};
+    // r.length=10;
+    // r.breadth=20;
     std::cout<<r.length<<std::endl;
     std::cout<<r.breadth<<std::endl;
+    struct emp e;
+    e.id=15;
+    strcpy(e.name, "manoj"); 
+    std::cout<<e.id<<std::endl;
+    std::cout<<e.name<<std::endl;
     return 0;
 }
